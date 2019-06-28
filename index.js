b2c0bd13a02fd6799d27d86399ac2c2d63073f70
Tish Chungoora <tishal82@gmail.com>
2019-06-28 13:12:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var recipes = {
 // Does not modify the original object, but rather returns a clone with the new data
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, { [key]: value })
+  return Object.assign({}, object, { [key]: value });
 }
 
 // Updates `object` with the given `key` and `value` (it is destructive) and returns the entire updated object
